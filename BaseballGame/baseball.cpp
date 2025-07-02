748ec4a9de639a838aadb69963c5bbb235741228
yalex-kim <yunbae.alex.kim@gmail.com>
2025-07-02 13:26:54 +0900

    feature/illegal argument refactoring

--- BaseballGame/baseball.cpp
+++ BaseballGame/baseball.cpp
@@ -5,16 +5,25 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+
+	}
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
 		if (guessNumber.length() != 3) {
 			throw length_error("Input must be exactly 3 characters long.");
 		}
-		if(guessNumber.find_first_not_of("0123456789") != string::npos) {
+		if (guessNumber.find_first_not_of("0123456789") != string::npos) {
 			throw invalid_argument("Input must only contain digits.");
 		}
-		if(guessNumber[0] == guessNumber[1] || 
-		   guessNumber[0] == guessNumber[2] || 
-		   guessNumber[1] == guessNumber[2]) {
-			throw domain_error("Digits must be unique.");
+		if (IsDuplicatedNumber(guessNumber)) {
+			throw invalid_argument("Digits must be unique.");
 		}
+	}
+	bool IsDuplicatedNumber(const string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1] ||
+			guessNumber[0] == guessNumber[2] ||
+			guessNumber[1] == guessNumber[2];
 	}
 };
